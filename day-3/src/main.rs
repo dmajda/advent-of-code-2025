dc7e17e911780bab23f1e50f2da21037930fa4f3
David Majda <david@majda.cz>
2025-12-03 19:29:33 +0100

    day-3: Pass `&[u8]` instead of `&str` to the `find_*` functions
    The funcions called `.bytes()` on the string slices anyway. Saving these
    calls speeds up the code a bit.

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -2,9 +2,9 @@ use std::io;
 
 use anyhow::Result;
 
-fn find_max_joltage(bank: &str) -> u32 {
+fn find_max_joltage(bank: &[u8]) -> u32 {
     debug_assert!(bank.len() >= 2);
-    debug_assert!(bank.bytes().all(|b| b >= b'0' && b <= b'9'));
+    debug_assert!(bank.iter().all(|&b| b >= b'0' && b <= b'9'));
 
     let (index1, joltage1) = find_max_battery(bank, 0, bank.len() - 1);
     let (_, joltage2) = find_max_battery(bank, index1 + 1, bank.len());
@@ -12,12 +12,12 @@ fn find_max_joltage(bank: &str) -> u32 {
     10 * joltage1 + joltage2
 }
 
-fn find_max_battery(bank: &str, start: usize, end: usize) -> (usize, u32) {
+fn find_max_battery(bank: &[u8], start: usize, end: usize) -> (usize, u32) {
     // The `rev` call is needed because `max_by_key` finds the *last* maximum
     // value and we want the *first* one.
 
     bank[start..end]
-        .bytes()
+        .iter()
         .enumerate()
         .rev()
         .max_by_key(|&(_, b)| b)
@@ -29,7 +29,7 @@ fn main() -> Result<()> {
     let mut joltage = 0;
 
     for line in io::stdin().lines() {
-        joltage += find_max_joltage(&line?);
+        joltage += find_max_joltage(line?.as_bytes());
     }
 
     println!("{}", joltage);
@@ -42,9 +42,9 @@ mod tests {
 
     #[test]
     fn find_max_joltage_works() {
-        assert_eq!(find_max_joltage("987654321111111"), 98);
-        assert_eq!(find_max_joltage("811111111111119"), 89);
-        assert_eq!(find_max_joltage("234234234234278"), 78);
-        assert_eq!(find_max_joltage("818181911112111"), 92);
+        assert_eq!(find_max_joltage(b"987654321111111"), 98);
+        assert_eq!(find_max_joltage(b"811111111111119"), 89);
+        assert_eq!(find_max_joltage(b"234234234234278"), 78);
+        assert_eq!(find_max_joltage(b"818181911112111"), 92);
     }
 }
